01a70c7d6da68956ce672caeb762646a8014f89e
maciiv <48811369+maciiv@users.noreply.github.com>
2022-01-25 15:24:16 +1000

    first commit

---
+++ lib/src/index.ts
@@ -0,0 +1,61 @@
+import * as d3 from "d3";
+
+/* ------------------------------------------------
+    Start data interfaces and classes 
+-------------------------------------------------- */
+
+interface IPactData {
+    capability: string;
+    colour: string;
+    correlation: number[];
+}
+
+export function buildVisualisation(data: IPactData[]){
+    let svg = d3.select("svg");
+    let matrix = data.map(d => d.correlation);
+
+    let chord = d3.chord()
+        .padAngle(0.05)
+        .sortSubgroups(d3.descending)
+        (matrix);
+    
+    svg.datum(chord)
+        .append("g")
+        .style("transform", "translate(500px, 500px)")
+        .selectAll("g")
+        .data(d => d.groups)
+        .join(
+            enter => enter.append("g")
+                        .call(g => g.append("path")
+                            .attr("id", (d, i) => data[i].capability)
+                            .style("fill", (d, i) => data[i].colour)
+                            .style("stroke", (d, i) => data[i].colour)
+                            .style("stroke-width", 1)
+                            .attr("d", d3.arc<d3.ChordGroup>()
+                                .innerRadius(200)
+                                .outerRadius(210)))
+                        .call(g => g.append("text")
+                            .append("textPath")
+                            .attr("xlink:href", (d, i) => "#" + data[i].capability)
+                            .attr("startOffset", "25%")
+                            .attr("dy", 10)
+                            .style("text-anchor", "middle")
+                            .style("font-size", "1.5rem")
+                            .text((d, i) => data[i].capability)),
+            update => update,
+            exit => exit
+        )
+    
+    svg.datum(chord)
+        .append("g")
+        .style("transform", "translate(500px, 500px)")
+        .selectAll("path")
+        .data(d => d)
+        .join("path")
+        .attr("d", d3.ribbon<d3.Chord, d3.ChordGroup>()
+            .radius(200))
+        .style("fill", d => data[d.source.index].colour)
+        .style("fill-opacity", 0.5)
+        .style("stroke", d => data[d.source.index].colour)
+        .style("stroke-width", 1)
+}
